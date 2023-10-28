64974f46246f9f378cc35f3ed0f14b990c5c4ebf
Kleiton Ewerton <kleiton.ewerton@estudante.ufjf.br>
2023-10-28 15:21:26 -0300

    alterando a ordem dos metodos; rename na 'divisao'; alterando retorno da 'soma'; alterando os parametros da 'soma'; adicionando parametro no 'notificar';

--- src/main/java/org/init/Main.java
+++ src/main/java/org/init/Main.java
@@ -5,27 +5,27 @@ package org.init;
 public class Main {
     public static void main(String[] args) {
 
-    soma(2,3);
+        soma(2.3,3.4);
     falar("Eu sou uma frase");
-    divisao(2,2);
-    notificar();
+    divisaoInteira(2,2);
+    notificar("Mensagem");
 
     }
 
-    public static int soma(int a, int b){
+    public static double soma(double a, double b){
         return a + b;
+    }
+
+    public static int divisaoInteira(int a, int b){
+        return a/b;
     }
 
     public static String falar(String frase){
         return "A pessoa esta falando que: " + frase;
     }
 
-    public static int divisao(int a, int b){
-        return a/b;
-    }
-
-    public static void notificar(){
-        System.out.println("Eu sou uma notificação");
+    public static void notificar(String extra){
+        System.out.println("Eu sou uma notificação! "+extra);
     }
 
 }
