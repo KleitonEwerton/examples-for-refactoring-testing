114ee3ea96b20560fe21bb4a487d3a3d6e9ca4ec
Kleiton Ewerton <kleiton.ewerton@estudante.ufjf.br>
2023-10-28 15:28:06 -0300

    criando pacote matematica; renomenando a classe main; movendo as funções matematicas para classe matematica; adicionando um novo paramentro no falar;

--- src/main/java/org/init/Main.java
+++ src/main/java/org/init/Main.java
@@ -1,27 +1,22 @@
 package org.init;
 
+import static org.matematica.Matematica.divisaoInteira;
+import static org.matematica.Matematica.soma;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
         soma(2.3,3.4);
-    falar("Eu sou uma frase");
-    divisaoInteira(2,2);
-    notificar("Mensagem");
+        falar("Eu sou uma frase", "João");
+        divisaoInteira(2,2);
+        notificar("Mensagem");
 
     }
 
-    public static double soma(double a, double b){
-        return a + b;
-    }
-
-    public static int divisaoInteira(int a, int b){
-        return a/b;
-    }
-
-    public static String falar(String frase){
-        return "A pessoa esta falando que: " + frase;
+    public static String falar(String frase, String pessoa){
+        return "A pessoa "+ pessoa +" esta falando que: " + frase;
     }
 
     public static void notificar(String extra){
---
+++ src/main/java/org/matematica/Matematica.java
@@ -0,0 +1,11 @@
+package org.matematica;
+
+public class Matematica {
+    public static double soma(double a, double b){
+        return a + b;
+    }
+
+    public static int divisaoInteira(int a, int b){
+        return a/b;
+    }
+}
