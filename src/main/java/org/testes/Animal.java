4552e643d55d76a606280bb0463081193d228985
Kleiton Ewerton <kleiton.ewerton@estudante.ufjf.br>
2023-11-01 21:40:27 -0300

    nova classe Animal novo pacote nova classe forma

---
+++ src/main/java/org/testes/Animal.java
@@ -0,0 +1,19 @@
+package org.testes;
+
+public class Animal {
+    String nome;
+    int idade;
+    String tipo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+}
---
+++ src/main/java/org/testes/Forma.java
@@ -0,0 +1,10 @@
+package org.testes;
+
+public class Forma {
+    int area;
+
+    public Forma(int area) {
+        this.area = area;
+    }
+
+}
