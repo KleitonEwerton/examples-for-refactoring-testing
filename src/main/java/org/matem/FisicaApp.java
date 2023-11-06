19f447b5e7eb934fbc1b87b1b1c3295c310fbba2
Kleiton Ewerton <kleiton.ewerton@estudante.ufjf.br>
2023-11-06 09:47:38 -0300

    modanca nos metodos

--- src/main/java/org/matem/FisicaApp.java
+++ src/main/java/org/matem/FisicaApp.java
@@ -11,38 +11,49 @@ public class FisicaApp {
     }
 
     public static double calcularDistancia(double vi, double t, double a) {
+        System.out.println("-------");System.out.println("calcularDistancia");System.out.println("-------");
         return (vi * t) + (0.5 * a * t * t);
     }
 
     public static double calcularVelocidadeFinal(double vi, double a, double t) {
+        System.out.println("-------");System.out.println("calcularVelocidadeFinal");System.out.println("-------");
         return vi + (a * t);
     }
 
     public static double calcularForca(double massa, double aceleracao) {
+        System.out.println("-------");System.out.println("calcularForca");System.out.println("-------");
         return massa * aceleracao;
     }
 
     public static double calcularEnergiaCinetica(double massa, double velocidade) {
+        System.out.println("-------");System.out.println("calcularEnergiaCinetica");System.out.println("-------");
         return 0.5 * massa * (velocidade * velocidade);
     }
 
     public static double calcularTrabalho(double forca, double distancia, double angulo) {
+        System.out.println("-------");System.out.println("calcularTrabalho");System.out.println("-------");
         return forca * distancia * Math.cos(Math.toRadians(angulo));
     }
 
     public static double calcularPressao(double forca, double area) {
+        System.out.println("-------");System.out.println("calcularPressao");System.out.println("-------");
         return forca / area;
     }
 
     public static double calcularDensidade(double massa, double volume) {
+        System.out.println("-------");System.out.println("calcularDensidade");System.out.println("-------");
         return massa / volume;
     }
 
     public static double calcularPotencia(double trabalho, double tempo) {
+        System.out.println("-------");System.out.println("calcularPotencia");System.out.println("-------");
         return trabalho / tempo;
     }
 
     public static double calcularLeiSegundaNewton(double forca, double massa) {
+        System.out.println("-------");
+        System.out.println("calcularLeiSegundaNewton");
+        System.out.println("-------");
         return forca / massa;
     }
 }
