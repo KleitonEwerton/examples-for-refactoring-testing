980c9e8c07b2dc77ea521bb49e6ff3be3ab7d18e
Kleiton Ewerton <kleiton.ewerton@estudante.ufjf.br>
2023-11-06 10:15:15 -0300

    9 inline

--- src/main/java/org/matem/Fisica.java
+++ src/main/java/org/matem/Fisica.java
@@ -7,7 +7,8 @@ public class Fisica extends Matematica{
     }
 
     public double calcularDistanciaEmTempo(double vi, double t, double a) {
-        return calcularDistanciaEmTempo2(vi, t, a);
+        System.out.println("-------");System.out.println("calcularDistancia");System.out.println("-------");
+        return (vi * t) + (0.5 * a * t * t);
     }
 
     public double calcularDistanciaEmTempo2(double vi, double t, double a) {
@@ -17,7 +18,8 @@ public class Fisica extends Matematica{
 
 
     public double calcularVelocidadeFinalRetonoDouble(double vi, double a, double t) {
-        return calcularVelocidadeFinalRetonoDouble2(vi, a,t);
+        System.out.println("-------");System.out.println("calcularVelocidadeFinal");System.out.println("-------");
+        return vi + (a * t);
     }
 
     public double calcularVelocidadeFinalRetonoDouble2(double vi, double a, double t) {
@@ -26,7 +28,8 @@ public class Fisica extends Matematica{
     }
 
     public double calcularForcaSobreMassa(double massa, double aceleracao) {
-        return calcularForcaSobreMassa2(massa, aceleracao);
+        System.out.println("-------");System.out.println("calcularForca");System.out.println("-------");
+        return massa * aceleracao;
     }
     public double calcularForcaSobreMassa2(double massa, double aceleracao) {
         System.out.println("-------");System.out.println("calcularForca");System.out.println("-------");
@@ -34,7 +37,8 @@ public class Fisica extends Matematica{
     }
 
     public double calcularEnergiaCineticaSobreMassa(double massa, double velocidade) {
-        return calcularEnergiaCineticaSobreMassa2(massa, velocidade);
+        System.out.println("-------");System.out.println("calcularEnergiaCinetica");System.out.println("-------");
+        return 0.5 * massa * (velocidade * velocidade);
     }
 
     public double calcularEnergiaCineticaSobreMassa2(double massa, double velocidade) {
@@ -43,7 +47,8 @@ public class Fisica extends Matematica{
     }
 
     public double calcularTrabalhoForca(double forca, double distancia, double angulo) {
-        return calcularTrabalhoForca2(forca, distancia, angulo);
+        System.out.println("-------");System.out.println("calcularTrabalho");System.out.println("-------");
+        return forca * distancia * Math.cos(Math.toRadians(angulo));
     }
 
     public double calcularTrabalhoForca2(double forca, double distancia, double angulo) {
@@ -52,7 +57,8 @@ public class Fisica extends Matematica{
     }
 
     public double calcularPressaoForca(double forca, double area) {
-        return calcularPressaoForca2( forca,  area);
+        System.out.println("-------");System.out.println("calcularPressao");System.out.println("-------");
+        return forca / area;
     }
 
     public double calcularPressaoForca2(double forca, double area) {
@@ -61,7 +67,8 @@ public class Fisica extends Matematica{
     }
 
     public double calcularDensidadeMassa(double massa, double volume) {
-        return  calcularDensidadeMassa2(massa, volume);
+        System.out.println("-------");System.out.println("calcularDensidade");System.out.println("-------");
+        return massa / volume;
     }
 
     public double calcularDensidadeMassa2(double massa, double volume) {
@@ -70,7 +77,8 @@ public class Fisica extends Matematica{
     }
 
     public double calcularPotenciaTrabalho(double trabalho, double tempo) {
-        return calcularPotenciaTrabalho2(trabalho, tempo);
+        System.out.println("-------");System.out.println("calcularPotencia");System.out.println("-------");
+        return trabalho / tempo;
     }
     public double calcularPotenciaTrabalho2(double trabalho, double tempo) {
         System.out.println("-------");System.out.println("calcularPotencia");System.out.println("-------");
@@ -78,7 +86,10 @@ public class Fisica extends Matematica{
     }
 
     public double calcularLeiSegundaNewtonForca(double forca, double massa) {
-        return calcularLeiSegundaNewtonForca2(forca, massa);
+        System.out.println("-------");
+        System.out.println("calcularLeiSegundaNewton");
+        System.out.println("-------");
+        return forca / massa;
     }
 
     public double calcularLeiSegundaNewtonForca2(double forca, double massa) {
