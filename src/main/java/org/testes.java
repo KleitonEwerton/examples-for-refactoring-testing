6581eb5ea7910d08ec31a15dcbeb79dfe5ede890
Kleiton Ewerton <kleitonewertonoliveira@gmail.com>
2024-06-11 12:03:42 -0300

    add teste in testes.java

--- src/main/java/org/testes.java
+++ src/main/java/org/testes.java
@@ -7,9 +7,28 @@ public class testes {
 
     /* this is commente */
 
-    int line = 9;
+    private int line;
     /*
      * block
      * comments
      */
+
+    // line
+    private String line;
+
+    public int getLine() {
+        return line;
+    }
+
+    public void setLine(int line) {
+        this.line = line;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public void setLine(int line) {
+        this.line = line;
+    }
 }
